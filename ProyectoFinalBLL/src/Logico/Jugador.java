8de50cbe00fe36ff4317c5e24858ac08597ad11f
diazs <diazs@PC>
2025-03-13 15:21:06 -0400

    Clase Jugador

--- ProyectoFinalBLL/src/Logico/Jugador.java
+++ ProyectoFinalBLL/src/Logico/Jugador.java
@@ -1,5 +1,10 @@
 package Logico;
 
 public class Jugador {
-
+	private String name;
+	private String direccion;
+	private String telefono;
+	private String id;
+	private boolean sexo;
+	private int puntos;
 }
